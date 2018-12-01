a2500ff4f2643471d699a5f80196883a9fe1ca33
lazusakan123123 <44501653+lazusakan123123@users.noreply.github.com>
2018-12-01 10:34:18 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -9,5 +9,5 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("online");
-client.user.setGame("?yardım | sürüm 0.0.1 güncellendi ", );
+client.user.setGame("+yardım | sürüm 0.0.1 güncellendi ", );
 }
